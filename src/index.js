ce23dc071c4515a5cddfd9535cbb357e4f2b99d7
Tiardo Wiechmann <205292868+TiardoWiechmann@users.noreply.github.com>
2026-04-18 18:51:15 +0200

    Hardcode html for contact

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 // index .js
-import { home } from "./home.js";
-import { menu } from "./menu.js"
-
-
+// import { home } from "./home.js";
+// import { menu } from "./menu.js";
+import "./contact.css";
 // const contentDiv = document.querySelector("#content");
 // contentDiv.appendChild(home());
 // console.log(contentDiv.textContent);
 
-menu();
+// menu();
