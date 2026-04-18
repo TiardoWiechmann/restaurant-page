c0c6426c5b80a86ef4bba75f1523d48800c9007e
Tiardo Wiechmann <205292868+TiardoWiechmann@users.noreply.github.com>
2026-04-18 19:42:02 +0200

    Implement contact module

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 // index .js
 // import { home } from "./home.js";
 // import { menu } from "./menu.js";
-import "./contact.css";
+import { contact } from "./contact.js";
+
 // const contentDiv = document.querySelector("#content");
 // contentDiv.appendChild(home());
 // console.log(contentDiv.textContent);
 
 // menu();
+contact();
